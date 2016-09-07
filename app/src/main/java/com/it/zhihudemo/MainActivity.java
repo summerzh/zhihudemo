c1f5502662f06a4e7bdc8c315817f5b05c2e13ad
unknown <>
2016-09-07 18:15:58 +0800

    9.7

--- app/src/main/java/com/it/zhihudemo/MainActivity.java
+++ app/src/main/java/com/it/zhihudemo/MainActivity.java
@@ -1,9 +1,12 @@
 package com.it.zhihudemo;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 
@@ -15,13 +18,19 @@ import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
 
-    private static final String TAG = "result";
-    private RecyclerView                 mRecyclerView;
-    private MyRecyclerAdapter            mMyRecyclerAdapter;
+    private static final String TAG       = "result";
+    private static final int PRELOAD_SIZE = 6;
+    private RecyclerView                       mRecyclerView;
+    private MyRecyclerAdapter                  mMyRecyclerAdapter;
+    private List<GirlInfo.ResultsEntity> mGirlsList;
+    private  Handler mHandler = new Handler();
+    private SwipeRefreshLayout mRefreshLayout;
+    private int mPage = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,70 +41,117 @@ public class MainActivity extends AppCompatActivity {
         initEvent();
     }
 
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        SwipeRefreshLayoutStart();
+    }
+
     private void initEvent() {
 
 
-//        final StaggeredGridLayoutManager manager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
-        final LinearLayoutManager llManager = new LinearLayoutManager(this);
-//        manager.setGapStrategy(StaggeredGridLayoutManager.GAP_HANDLING_NONE);
-        mRecyclerView.setLayoutManager(llManager);
-//        mRecyclerView.addItemDecoration(new DividerGridItemDecoration(getContext()));
-//        mRecyclerView.setPadding(0, 0, 0, 0);
-//        mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
-//            @Override
-//            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
-//                super.onScrollStateChanged(recyclerView, newState);
-////                llManager.invalidateSpanAssignments();
-//            }
-//        });
+        StaggeredGridLayoutManager manager = new StaggeredGridLayoutManager(2,
+                1);
+        //
+        mRecyclerView.setLayoutManager(manager);
+        mGirlsList = new ArrayList<>();
+        mMyRecyclerAdapter = new MyRecyclerAdapter(this, mGirlsList);
+        mRecyclerView.setAdapter(mMyRecyclerAdapter);
+        mRecyclerView.addOnScrollListener(getOnBottomListener(manager));
 
-        requestData();
+        mRefreshLayout.setOnRefreshListener(this);
+    }
 
+    private RecyclerView.OnScrollListener getOnBottomListener(final StaggeredGridLayoutManager manager) {
+        return new RecyclerView.OnScrollListener(){
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                // 判断是否滑倒底部,findLastCompletelyVisibleItemPositions得到的数是两列图片中后面一列图片
+                // 完全显示出来的index
+                boolean isBottom =
+                        manager.findLastCompletelyVisibleItemPositions(new int[2])[1] >=
+                                mMyRecyclerAdapter.getItemCount() - PRELOAD_SIZE;
+                //                Log.d(TAG, manager.findLastCompletelyVisibleItemPositions(new int[2])[1] + "");
+                Log.d(TAG, mMyRecyclerAdapter.getItemCount() - PRELOAD_SIZE + "");
+                if (!mRefreshLayout.isRefreshing() && isBottom) {
+                    SwipeRefreshLayoutStart();
+                }
+            }
+        };
     }
 
     private void initView() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
-
+        mRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh);
         setSupportActionBar(toolbar);
+        mRefreshLayout.setColorSchemeResources(R.color.swipe_color_1, R.color.swipe_color_2, R.color.swipe_color_3);
     }
 
 
     private void requestData() {
         OkHttpClient client = new OkHttpClient();
         final Request request = new Request.Builder()
-                .url("http://gank.io/api/data/福利/10/1")
+                .url("http://gank.io/api/data/福利/10/" + mPage)
                 .build();
-
-
         Call call = client.newCall(request);
         call.enqueue(new Callback() {
 
             @Override
             public void onFailure(Request request, IOException e) {
+                SwipeRefreshLayoutStop();
                 e.printStackTrace();
-                Log.d(TAG, "onFailure ");
             }
 
             @Override
             public void onResponse(Response response) throws IOException {
-                Log.d(TAG, "onResponse ");
+
                 String string = response.body().string();
                 Gson gson = new Gson();
                 GirlInfo girlInfo = gson.fromJson(string, GirlInfo.class);
-                final List<GirlInfo.ResultsEntity> results = girlInfo.getResults();
 
-                runOnUiThread(new Runnable() {
+                mGirlsList.addAll(girlInfo.getResults());
+                mHandler.post(new Runnable() {
                     @Override
                     public void run() {
-                        mMyRecyclerAdapter = new MyRecyclerAdapter(MainActivity.this, results);
-                        mRecyclerView.setAdapter(mMyRecyclerAdapter);
+                        mMyRecyclerAdapter.notifyDataSetChanged();
+                        if (mRefreshLayout.isRefreshing()) {
+                            SwipeRefreshLayoutStop();
+                        }
                     }
                 });
             }
         });
+
+
     }
 
+    public void SwipeRefreshLayoutStop() {
+        mRefreshLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                mRefreshLayout.setRefreshing(false);
+            }
+        });
 
+
+    }
+
+    public void SwipeRefreshLayoutStart() {
+        mRefreshLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                mRefreshLayout.setRefreshing(true);
+                onRefresh();
+            }
+        });
+    }
+
+    @Override
+    public void onRefresh() {
+        mPage++;
+        requestData();
+    }
 }
 
--- app/src/main/java/com/it/zhihudemo/MyRecyclerAdapter.java
+++ app/src/main/java/com/it/zhihudemo/MyRecyclerAdapter.java
@@ -4,10 +4,9 @@ import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
 
-import com.squareup.picasso.Picasso;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 
 import java.util.List;
 
@@ -36,11 +35,13 @@ public class MyRecyclerAdapter extends RecyclerView.Adapter<MyRecyclerAdapter.Vi
     public void onBindViewHolder(ViewHolder ViewHolder, int position) {
 
         if (mResults != null) {
-            Picasso.with(mContext).load(mResults.get(position).getUrl())
-                    .placeholder(R.mipmap.ic_launcher)
+            Glide.with(mContext)
+                    .load(mResults.get(position).getUrl())
+                    .placeholder(R.color.white)
+                    .fitCenter()
+                    .diskCacheStrategy(DiskCacheStrategy.ALL)
                     .error(R.mipmap.ic_launcher)
                     .into(ViewHolder.mImageView);
-            ViewHolder.mTextView.setText(mResults.get(position).getWho());
         }
     }
 
@@ -54,15 +55,13 @@ public class MyRecyclerAdapter extends RecyclerView.Adapter<MyRecyclerAdapter.Vi
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
 
-        private ImageView mImageView;
-        private TextView  mTextView;
+        private RatioImageView mImageView;
 
         public ViewHolder(View itemView) {
             super(itemView);
 
-            mImageView = (ImageView) itemView.findViewById(R.id.iv_girl);
-            mTextView = (TextView) itemView.findViewById(R.id.tv_des);
-
+            mImageView = (RatioImageView) itemView.findViewById(R.id.iv_girl);
+            mImageView.setOriginalSize(50, 50);
         }
     }
 }
